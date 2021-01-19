0a3319cdb406767cad1b561b1a074df6841e1d87
Scott Wedge <scott.wedge@gmail.com>
2021-01-18 21:34:32 -0500

    Initial commit of project #1

---
+++ 1_LetterCounter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Project 1 prompts for a line of text to be entered
+# and then prompts for which letter count is desired.
+
+# Instructions are that both upper case and lower case are counted for the same letter.
+
+
