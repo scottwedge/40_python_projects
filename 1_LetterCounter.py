f69c7a46fec21f8263010c6dd1eec918543c6590
Scott Wedge <scott.wedge@gmail.com>
2021-01-18 21:45:35 -0500

    Improve description for project 1 (count occurrences of desired letter in line of text)

--- 1_LetterCounter.py
+++ 1_LetterCounter.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-# Project 1 prompts for a line of text to be entered
-# and then prompts for which letter count is desired.
+# Project 1: 
+# - prompts for user name and capitalizes it if needed
+# - prompts for a line of text to be entered
+# - prompts for which letter count is desired.
+# - prints letter and count for that letter
 
 # Instructions are that both upper case and lower case are counted for the same letter.
 
