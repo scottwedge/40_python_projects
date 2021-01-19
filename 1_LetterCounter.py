ab5ce97520c5c2b1fb46e11ab1d0cf3a90c03702
Scott Wedge <scott.wedge@gmail.com>
2021-01-18 22:06:18 -0500

    Basic program works so far

--- 1_LetterCounter.py
+++ 1_LetterCounter.py
@@ -8,4 +8,18 @@
 
 # Instructions are that both upper case and lower case are counted for the same letter.
 
+# Variables:
+# name: entered name of user
+# line: text string to be evaluated
+# letter: letter to be counted in line
+# count: number of occurrences of letter in line
+
+# Prompt for user name, capitalize to be safe
+name = input("What is your name? ")
+name = name.capitalize()
+
+print("Welcome", name)
+line = input("Please enter the line of text to be evaluated: ")
+
+print("Thank you for entering:", line)
 
