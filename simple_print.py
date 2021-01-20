8ef0c7d1de5cba02e86811f2c8885c6f2276addd
Scott Wedge <scott.wedge@gmail.com>
2021-01-20 18:24:01 -0500

    print format statement using variables

--- simple_print.py
+++ simple_print.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
 print("Hello there {} I just wanted to say {}.".format("Steve", "hi"))
+
+name1 = "David"
+action1 = "goodbye"
+print("Hello {} I need to say {} now.".format(name1, action1))
