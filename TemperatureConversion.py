759776a9ca7a77bf426719778767e84b01a613e9
Scott Wedge <scott.wedge@gmail.com>
2021-01-20 21:07:42 -0500

    Initial commit of temperature conversion program. Has program description and comments for each section of code.

---
+++ TemperatureConversion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Script to convert temperature from Fahrenheit to Celcius and Kelvin
+# Results to be accurate to four decimal places and aligned in a table
+
+
+# Print welcome and purpose of application
+print("Welcome to the temperature conversion application.")
+
+# Get user value in Fahrenheit
+f_temp = input("Please enter the temperature in Fahrenheit")
+
+# Convert temperature to Celcius
+
+
+# Convert temperature to Kelvin
+
+
+# Display all three values, with results accurate to four decimal places.
+
+
