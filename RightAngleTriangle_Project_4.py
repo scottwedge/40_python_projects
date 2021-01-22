8ce75f88a180c04d4da7edbf085860764e44bb52
Scott Wedge <scott.wedge@gmail.com>
2021-01-21 22:34:42 -0500

    Initial commit for project #4 about right angle triangle with project description and a comment for each section of code.

---
+++ RightAngleTriangle_Project_4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Project 4 involves right angle triangle
+# Need to calculate hypotenuse and area of triangle.
+
+# Welcome user, then prompt for length of two sides.
+# Then display hypotenuse and area accurate to three decimal spaces.
+
+
+# Welcome user and state purpose of application.
+
+# Prompt for first side length.
+
+# Prompt for second side length.
+
+# Display length of hypotenuse accurate to three decimal places.
+
+# Display area of triangle accurate to three decimal places.
+
+
