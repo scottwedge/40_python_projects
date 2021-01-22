8a8c8a568a01735f1065c1717d75f91b032207fc
Scott Wedge <scott.wedge@gmail.com>
2021-01-21 22:42:03 -0500

    First attempt at calculations and displaying results

--- RightAngleTriangle_Project_4.py
+++ RightAngleTriangle_Project_4.py
@@ -8,13 +8,24 @@
 
 
 # Welcome user and state purpose of application.
+print("Welcome to the right angle triangle application")
 
 # Prompt for first side length.
+user_side_1 = input("Please enter length of first side")
+side1 = float(user_side_1)
 
 # Prompt for second side length.
+user_side_2 = input("Please enter length of second side")
+side2 = float(user_side_2)
 
+# Calculate hypotenuse (square root of sum of square of other two sides)
+hypotenuse = (side1**2 + side2**2)**0.5
 # Display length of hypotenuse accurate to three decimal places.
+print("Hypotenuse length is {:3f}.".format(hypotenuse))
+
+# Calculate area of triangle (half of one side multiply by other side)
+area = 0.5 * side1 * side2
 
 # Display area of triangle accurate to three decimal places.
-
+print("Area is {:3f}.".format(area))
 
