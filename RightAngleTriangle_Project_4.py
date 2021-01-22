4faf78fe5d6c8548d2aeb28e7231c6ac5be73854
Scott Wedge <scott.wedge@gmail.com>
2021-01-21 22:56:57 -0500

    Convert calculations to functions

--- RightAngleTriangle_Project_4.py
+++ RightAngleTriangle_Project_4.py
@@ -6,6 +6,14 @@
 # Welcome user, then prompt for length of two sides.
 # Then display hypotenuse and area accurate to three decimal spaces.
 
+# Functions
+def calc_hyp(a, b):
+    hyp = (a**2 + b**2) ** 0.5
+    return hyp
+
+def calc_area(a, b):
+    area = 0.5 * a * b
+    return area
 
 # Welcome user and state purpose of application.
 print("Welcome to the right angle triangle application")
@@ -19,12 +27,13 @@ user_side_2 = input("What is the second leg of the triangle: ")
 side2 = float(user_side_2)
 
 # Calculate hypotenuse (square root of sum of square of other two sides)
-hypotenuse = (side1**2 + side2**2)**0.5
+hypotenuse = calc_hyp(side1, side2)
+
 # Display length of hypotenuse accurate to three decimal places.
 print("For a triangle with legs of {} and {} the hypotenuse is {:.3f}.".format(side1, side2, hypotenuse))
 
 # Calculate area of triangle (half of one side multiply by other side)
-area = 0.5 * side1 * side2
+area = calc_area(side1, side2)
 
 # Display area of triangle accurate to three decimal places.
 print("For a triangle with legs of {} and {} the area is {:.3f}.".format(side1, side2, area))
