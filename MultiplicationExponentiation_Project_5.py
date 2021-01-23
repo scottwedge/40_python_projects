e8467637254b77f00f4592d74d540e0854071d3d
Scott Wedge <scott.wedge@gmail.com>
2021-01-22 22:24:13 -0500

    Add ending phrases - indented using spaces

--- MultiplicationExponentiation_Project_5.py
+++ MultiplicationExponentiation_Project_5.py
@@ -3,6 +3,9 @@
 # Script to print both multiplication and exponentiation table
 # From range of 1 to 9 inclusive.
 
+# Imports
+import string # to access capwords method in string module
+
 # Print welcome message 
 print("Welcome to the Multiplication/Exponentiation application.")
 
@@ -29,5 +32,8 @@ print()  # blank line
 print("Exponentiation table for", user_number)
 
 # Print math is cool four times
-phrase = "math is cool"
-print(user_name, phrase)
+phrase = "math is cool!"
+print(user_name.capitalize(), phrase.capitalize())
+print("    ", user_name.lower(), phrase.lower())
+print("        ", user_name.capitalize(), string.capwords(phrase))
+print("            ", user_name.upper(), phrase.upper())
