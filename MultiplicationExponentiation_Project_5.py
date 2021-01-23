52f3ebdc7046367d86bcd13c466b1baba9cc3c68
Scott Wedge <scott.wedge@gmail.com>
2021-01-22 21:45:32 -0500

    Convert user input to float, add ending statement of user name plus 'math is cool\!'

--- MultiplicationExponentiation_Project_5.py
+++ MultiplicationExponentiation_Project_5.py
@@ -12,16 +12,22 @@ user_name = input("What is your name: ")
 # Prompt for number to work with
 user_number = input("What number do you want to work with: ")
 
+# Convert input to float number (from character string)
+num = float(user_number)
+
 
 # Display multiplication table for 1 through 9
 print() # blank line
 print("Multiplication table for", user_number)
 
 for n in range(9):
-    print(n," *", user_number, "=", user_number*n)
+    print(n," *", num, "=", num * n)
     
 
 # Display exponentiation table for 1 through 9
 print()  # blank line
 print("Exponentiation table for", user_number)
 
+# Print math is cool four times
+phrase = "math is cool"
+print(user_name, phrase)
