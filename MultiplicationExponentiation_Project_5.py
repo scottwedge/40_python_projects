262e908c4af912651b1e8f675b65519e21bbeac7
Scott Wedge <scott.wedge@gmail.com>
2021-01-22 20:36:23 -0500

    Initial commit for project 5 (Multiplication and Exponentiation table)

---
+++ MultiplicationExponentiation_Project_5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Script to print both multiplication and exponentiation table
+# From range of 1 to 9 inclusive.
+
+# Print welcome message 
+print("Welcome to the Multiplication/Exponentiation application.")
+
+# Prompt for user name
+user_name = input("What is your name: ")
+
+# Prompt for number to work with
+user_number = input("What number do you want to work with: ")
+
+# Display multiplication table for 1 through 9
+print() # blank line
+print("Multiplication table for", user_number)
+
+# Display exponentiation table for 1 through 9
+print()  # blank line
+print("Exponentiation table for", user_number)
+
