667349bc6065adfd2dcee92ca2da088748850f40
Scott Wedge <scott.wedge@gmail.com>
2021-01-23 21:44:29 -0500

    Successfully prompt for the four grades and add to list

--- GradeSorter_Lists_Challenge_1.py
+++ GradeSorter_Lists_Challenge_1.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 # Print welcome message
+print("Welcome to the grade sorter application")
+print() # blank line
+
+# initialize empty list of grades
+grades_list = []
 
 # Prompt for a grade four times
+for j in ["first", "second", "third", "fourth"]:
+    grade = input("What is your {} grade (0-100): ".format(j))
+    grades_list.append(grade)
 
 # List the four grades
 
