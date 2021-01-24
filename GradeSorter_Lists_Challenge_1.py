b722b6a9ead0e0e58e21e11d84ddbdac5d5ca20a
Scott Wedge <scott.wedge@gmail.com>
2021-01-23 21:10:42 -0500

    First draft with comments for each section of code for Grade Sorter application

---
+++ GradeSorter_Lists_Challenge_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Print welcome message
+
+# Prompt for a grade four times
+
+# List the four grades
+
+# Display the grades sorted from highest to lowest
+
+# State and list the two lowest grades will be removed
+
+# Display the remaining grades
+
+# Comment on the highest grade
