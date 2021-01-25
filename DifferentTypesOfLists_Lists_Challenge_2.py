80297c55d08a72deae97b87179ef0bd390450238
Scott Wedge <scott.wedge@gmail.com>
2021-01-24 19:33:25 -0500

    Initial commit with comments for program purpose and sections

---
+++ DifferentTypesOfLists_Lists_Challenge_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Highlight similarities and differences of four types of lists 
+# Lists of strings, integers, floats and lists.
+# For each list, compare its data type, the elements of the list, and the type of the first element.
+
+# Then sort each list numerically and alphabetically
+
+# Print welcome message of "Summary Table"
+
+num_strings = ["15", "100", "55", "43"]
+
+num_ints = [15, 100, 55, 42]
+
+num_floats = [2.2, 5.0, 1.245, 0.142857] 
+
+num_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+# Print welcome message of "Summary Table"
+title = "Summary Table"
+centred_title = title.center(80)
+print(centred_title)
+
