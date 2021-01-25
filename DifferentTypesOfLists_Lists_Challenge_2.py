e75e74160c1794707a4de19511eabcb351252f5b
Scott Wedge <scott.wedge@gmail.com>
2021-01-24 21:10:33 -0500

    Correctly printing variable name and contents

--- DifferentTypesOfLists_Lists_Challenge_2.py
+++ DifferentTypesOfLists_Lists_Challenge_2.py
@@ -11,7 +11,8 @@ num_strings = ["15", "100", "55", "43"]
 num_ints = [15, 100, 55, 42]
 num_floats = [2.2, 5.0, 1.245, 0.142857] 
 num_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-list_of_lists = [num_strings, num_ints, num_floats, num_lists]
+list_of_list_names = ["num_strings", "num_ints", "num_floats", "num_lists"]
+list_of_list_contents = [num_strings, num_ints, num_floats, num_lists]
 
 
 # Print welcome message of "Summary Table", centred on line
@@ -20,8 +21,9 @@ centred_title = title.center(80)
 print(centred_title)
 
 # Cycle through each list
-for j in list_of_lists:
-    print(j)
-#    print("The variable {} is a {}.".format(j, type(j))
-#    print("It contains the elements: {}".format(list_of_lists[j]))
+for num in range(len(list_of_list_names)):
+#    print(list_of_list_names[num], type(list_of_list_contents[num]))
+#for num in range(list_of_lists):
+    print("The variable {} is a {}.".format(list_of_list_names[num], type(list_of_list_contents[num])))
+    print("It contains the elements: {}".format(list_of_list_contents[num]))
 #    print("The first element {} is a {}.".format(list_of_lists[j].[0], type(list_of_lists[j].[0]))
