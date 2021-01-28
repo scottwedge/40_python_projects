71f0404df2dab6779fb8d1332eae325c5faf231b
Scott Wedge <scott.wedge@gmail.com>
2021-01-27 22:40:25 -0500

    Initial commit; comments explain purpose and actions in script

---
+++ GroceryListApp_List_Challenge_3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Script that will prompt for items to add to shopping list until there are five items
+# Sort list alphabetically and display the list.
+# Then remove three items (spelled in any case) from list when prompted
+# When two items remain, say requested purchase not available
+# Remove it from list and add another item
+
+
