05bab3e400b0cd3b7e74e85217f7eef250660975
Scott Wedge <scott.wedge@gmail.com>
2021-01-29 23:31:21 -0500

    Create list of roster positions

--- BasketBallRosterProgram_List_Challenge_4.py
+++ BasketBallRosterProgram_List_Challenge_4.py
@@ -8,3 +8,15 @@
 # Display count and aligned list of position and names of final team
 
 
+# Welcome user
+print("Welcome to the Basketball Roster program.")
+print() # blank line
+
+# List roster positions
+roster = ["point guard", "shooting guard","small forward","power forward","center"]
+
+# Prompt for each position (point guard, shooting guard, small forward
+# power forward, center)
+
+for j in roster:
+    print(j)
