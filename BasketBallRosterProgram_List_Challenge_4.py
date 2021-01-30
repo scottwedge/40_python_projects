ad26b479abf65ef055ca593f3b1c8becd971ddce
Scott Wedge <scott.wedge@gmail.com>
2021-01-29 23:18:58 -0500

    Initial commit with comments about program operation

---
+++ BasketBallRosterProgram_List_Challenge_4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Welcome user to program
+# Prompt for name for each basketball position
+# Display aligned column lists for position and player name
+# Announce one player is injured
+# Prompt for replacement for that position
+# Display count and aligned list of position and names of final team
+
+
