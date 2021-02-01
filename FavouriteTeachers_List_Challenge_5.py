2f2257a5e5b5d1457ff46bb55f87969beff69638
Scott Wedge <scott.wedge@gmail.com>
2021-01-31 22:24:25 -0500

    Initial commit of favourite teach script

---
+++ FavouriteTeachers_List_Challenge_5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Favourite teachers program
+
+# Welcome user to Favourite Teachers Program
+# Create list by prompting for teachers' first names in any format.
+# Then list teachers in order entered in title format
+# Then list teachers alphabetically
+# Then list them in reverse alphabetical order.
+# Then list top two teachers.
+# Then list the next two favourites.
+# Then list the last favourite.
+# Then list the total number.
+# Then warn that first is no longer your favourite and prompt for new FAVOUITE
+# Then list all teachers in favourite, alphabetical and reverse alphabetical orders and total number of teachers
+# Then prompt to remove one teacher and relist allo
+
+ 
+
