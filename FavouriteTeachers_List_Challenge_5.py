614de81af7da60c4ab7aa0ebe2d0f4fbf0106c95
Scott Wedge <scott.wedge@gmail.com>
2021-02-01 22:41:13 -0500

    Move code into 'main'; move common code into functions

--- FavouriteTeachers_List_Challenge_5.py
+++ FavouriteTeachers_List_Challenge_5.py
@@ -4,6 +4,7 @@
 
 # Welcome user to Favourite Teachers Program
 # Create list by prompting for teachers' first names in any format.
+# (Convert and store names in title format)
 # Then list teachers in order entered in title format
 # Then list teachers alphabetically
 # Then list them in reverse alphabetical order.
@@ -11,72 +12,104 @@
 # Then list the next two favourites.
 # Then list the last favourite.
 # Then list the total number.
-# Then warn that first is no longer your favourite and prompt for new FAVOUITE
+# Then warn that first is no longer your favourite and prompt for new FAVORITE
 # Then list all teachers in favourite, alphabetical and reverse alphabetical orders and total number of teachers
-# Then prompt to remove one teacher and relist allo
+# Then prompt to remove one teacher and relist all
 
+
+# Functions
+def get_names():
+    teacher_list = [] # initialize blank list
+    count_list = ["first", "second", "third", "fourth"]
+    for j in count_list:
+        teacher_input = input("Who is your {} favourite teacher: ".format(j))
+        teacher = teacher_input.title() # convert to title format
+        teacher_list.append(teacher)
+    return teacher_list
+
+def sort_alpha(teacher_list):
+    teacher_alpha = teacher_list.copy()
+    teacher_alpha.sort() # sort alphabetically
+    return teacher_alpha
+
+def sort_rev_alpha(teacher_alpha):
+    teacher_rev_alpha = teacher_alpha.copy()
+    teacher_rev_alpha.reverse()
+    return teacher_rev_alpha
+        
  
-print("Welcome to the Favourite Teachers Program")
-print() # blank line
-
-# get first four favourite teachers
-teacher_list = [] # initialize blank list
-count_list = ["first", "second", "third", "fourth"]
-for j in count_list:
-    teacher_input = input("Who is your {} favourite teacher: ".format(j))
+def main()"
+    print("Welcome to the Favourite Teachers Program")
+    print() # blank line
     
-    teacher = teacher_input.title() # convert to title format
-    teacher_list.append(teacher)
+    # get first four favourite teachers
+    teacher_list = get_names()
+
+    # Create list sorted alphabetically
+        teacher_alpha = teacher_list.copy()
+        teacher_alpha.sort() # sort alphabetically
+        teacher_revalpha = teacher_alpha.copy()
+        teacher_revalpha.reverse() # sort reverse alphabetically
+        
+    # Display teachers in different orders
+    print() # blank line
+    print("Your favourite teachers ranked are:", teacher_list)  # list in order of entry
+    print("Your favourite teachers alphabetically are: {}". format(teacher_alpha))
+    print("Your favourite teachers in reverse alphabetical order are: {}".format(teacher_revalpha))
+    
+    # Then list the top two teachers
+    print() # blank line
+    print("Your top two teachers are: {} and {}".format(teacher_list[0], teacher_list[1]))
+    
+    # Then list the next two teachers
+    print("The next two teachers are: {} and {}".format(teacher_list[2], teacher_list[3]))
+    
+    # Then list the last teacher
+    print("Your last favourite teacher is: {}".format(teacher_list[-1]))
+    
+    # Display number of teachers in list
+    print("Your have a total of {} favourite teachers.".format(len(teacher_list)))
+    
+    # Get a new favourite teacher
+    new_teacher = input("Oops {} is no longer your favourite teacher. Who is your new FAVOURITE teacher: ".format(teacher_list[0]))
+    new_teacher = new_teacher.title() # convert to title format
+    teacher_list.insert(0, new_teacher)  # Place new name first in list
+    
+    # Sort lists again 
     teacher_alpha = teacher_list.copy()
     teacher_alpha.sort() # sort alphabetically
     teacher_revalpha = teacher_alpha.copy()
     teacher_revalpha.reverse() # sort reverse alphabetically
     
-# Display teachers in different orders
-print() # blank line
-print("Your favourite teachers ranked are:", teacher_list)  # list in order of entry
-print("Your favourite teachers alphabetically are: {}". format(teacher_alpha))
-print("Your favourite teachers in reverse alphabetical order are: {}".format(teacher_revalpha))
+    # Display teachers in different orders
+    print() # blank line
+    print("Your favourite teachers ranked are:", teacher_list)  # list in order of entry
+    print("Your favourite teachers alphabetically are: {}". format(teacher_alpha))
+    print("Your favourite teachers in reverse alphabetical order are: {}".format(teacher_revalpha))
+    
+    # Then list the top two teachers
+    print() # blank line
+    print("Your top two teachers are: {} and {}".format(teacher_list[0], teacher_list[1]))
+    
+    # Then list the next two teachers
+    print("The next two teachers are: {} and {}".format(teacher_list[2], teacher_list[3]))
+    
+    # Then list the last teacher
+    print("Your last favourite teacher is: {}".format(teacher_list[-1]))
+    
+    # Display number of teachers in list
+    print("Your have a total of {} favourite teachers.".format(len(teacher_list)))
+    
+    # Remove a teacher
+    removed_teacher = input("You've decided that you no longer like a teacher. Which teacher would you like to remove from the list: ")
+    teacher_list.remove(removed_teacher)
+    
+    # Display in order
+    
+    # resort alphabetically and display
+    
+    # resort reverse alphabetically and display
 
-# Then list the top two teachers
-print() # blank line
-print("Your top two teachers are: {} and {}".format(teacher_list[0], teacher_list[1]))
 
-# Then list the next two teachers
-print("The next two teachers are: {} and {}".format(teacher_list[2], teacher_list[3]))
-
-# Then list the last teacher
-print("Your last favourite teacher is: {}".format(teacher_list[-1]))
-
-# Display number of teachers in list
-print("Your have a total of {} favourite teachers.".format(len(teacher_list)))
-
-# Get a new favourite teacher
-new_teacher = input("Oops {} is no longer your favourite teacher. Who is your new FAVOURITE teacher: ".format(teacher_list[0]))
-new_teacher = new_teacher.title() # convert to title format
-teacher_list.insert(0, new_teacher)  # Place new name first in list
-
-# Sort lists again 
-teacher_alpha = teacher_list.copy()
-teacher_alpha.sort() # sort alphabetically
-teacher_revalpha = teacher_alpha.copy()
-teacher_revalpha.reverse() # sort reverse alphabetically
-
-# Display teachers in different orders
-print() # blank line
-print("Your favourite teachers ranked are:", teacher_list)  # list in order of entry
-print("Your favourite teachers alphabetically are: {}". format(teacher_alpha))
-print("Your favourite teachers in reverse alphabetical order are: {}".format(teacher_revalpha))
-
-# Then list the top two teachers
-print() # blank line
-print("Your top two teachers are: {} and {}".format(teacher_list[0], teacher_list[1]))
-
-# Then list the next two teachers
-print("The next two teachers are: {} and {}".format(teacher_list[2], teacher_list[3]))
-
-# Then list the last teacher
-print("Your last favourite teacher is: {}".format(teacher_list[-1]))
-
-# Display number of teachers in list
-print("Your have a total of {} favourite teachers.".format(len(teacher_list)))
+if __name__ == "__main__":
+    main()
