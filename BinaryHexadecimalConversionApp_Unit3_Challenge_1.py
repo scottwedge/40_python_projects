5f8aee5d6723d3f2a13fddcd56c90f3b537cc91f
Scott Wedge <scott.wedge@gmail.com>
2021-02-02 20:10:34 -0500

    Initial commit for binary and hexadecimal conversion program with comment for each section of code

---
+++ BinaryHexadecimalConversionApp_Unit3_Challenge_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Print welcome header
+# Prompt for decimal number
+# Generate lists
+# Using slices, show a portion of each list
+# Prompt for decimal value to start at
+# Prompt for decimal number to stop at
+# List decimal values in that portion
+# List binary values in that portion
+# List hexadecimal values in that portion
+# Prompt for user to press enter to display all values
+# using format of decimal number then binary then hexadecimal on same line separated by "----"
+
