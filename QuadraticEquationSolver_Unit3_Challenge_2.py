eee9e906f663a8ce95306708f47bb629687f245d
Scott Wedge <scott.wedge@gmail.com>
2021-02-03 22:51:51 -0500

    Create loop to read a b and c values for each quadratic equation

--- QuadraticEquationSolver_Unit3_Challenge_2.py
+++ QuadraticEquationSolver_Unit3_Challenge_2.py
@@ -14,5 +14,17 @@ print("A complex number has two parts: a + bj")
 print("Where a is the real portion and bj is the imaginary portion.")
 
 print()
-count = input("How many equations would you like to solve today: ")
+num_eq = input("How many equations would you like to solve today: ")
+num_eq = int(num_eq) # convert string input to integer
+
+for j in range(1, num_eq + 1):
+    print() # blank line
+    print("Solving equation #{}".format(j))
+    print("----------------------------------------------------")
+    print() # blank line
+    a = input("Please enter your value of a (coefficient of x**2): ")
+    b = input("Please enter your value of b (coefficient of x): ")
+    c = input("Please enter your value of c (coefficient): ")
+    print() # blank line
+    print("Your solutions to {}x**2 + {}x + {} are:".format(a, b, c))
 
