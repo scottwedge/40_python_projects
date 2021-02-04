3bece2a3187a098ea579210b8efc354f9884610c
Scott Wedge <scott.wedge@gmail.com>
2021-02-03 22:41:42 -0500

    Initial commit with initial comments describing a quadratic equation

---
+++ QuadraticEquationSolver_Unit3_Challenge_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Welcome banner
+# Explain format of a quadratic equation 
+# Ask how many equations will be solved today.
+
+
+print("Welcome to the Quadratic Solver App.")
+print() # blank line
+
+print("A quadratic equation is of the form ax**2 + bx + c = 0")
+print("Your solutions can be real or complex numbers.")
+print("A complex number has two parts: a + bj")
+print("Where a is the real portion and bj is the imaginary portion.")
+
+print()
+count = input("How many equations would you like to solve today: ")
+
