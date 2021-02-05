50891a3e545c5427d3eaae043cf84a196bc8135e
Scott Wedge <scott.wedge@gmail.com>
2021-02-04 23:37:37 -0500

    Move welcome statement and quadratic equation explanation into a function

--- QuadraticEquationSolver_Unit3_Challenge_2.py
+++ QuadraticEquationSolver_Unit3_Challenge_2.py
@@ -8,16 +8,20 @@
 
 import cmath
 
-def main():
+def welcome():
     print("Welcome to the Quadratic Solver App.")
     print() # blank line
-    
     print("A quadratic equation is of the form ax**2 + bx + c = 0")
     print("Your solutions can be real or complex numbers.")
     print("A complex number has two parts: a + bj")
     print("Where a is the real portion and bj is the imaginary portion.")
-    
     print()
+
+
+def main():
+    # print welcome and explain quadratic equations
+    welcome()
+
     num_eq = input("How many equations would you like to solve today: ")
     num_eq = int(num_eq) # convert string input to integer
     
