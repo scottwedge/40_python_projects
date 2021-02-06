9803600860d02701042659b9785b072d5244e878
Scott Wedge <scott.wedge@gmail.com>
2021-02-05 22:05:27 -0500

    Initial commit with comment for each section of code

---
+++ FactorialChallenge_ForLoops_Challenge_3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# print Welcome header
+# Prompt for factorial number
+# Display how that factorial is calculated
+# Show result from math library
+# Show result from own algorithm.
+# Verify that both results are the same.
+
+
