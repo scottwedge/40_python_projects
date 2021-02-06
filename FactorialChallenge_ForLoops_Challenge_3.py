8285b1467d90b50ae7ac9f3781701629e41b7a8a
Scott Wedge <scott.wedge@gmail.com>
2021-02-05 22:20:14 -0500

    Prompt for user input and display how factorial is calculated

--- FactorialChallenge_ForLoops_Challenge_3.py
+++ FactorialChallenge_ForLoops_Challenge_3.py
@@ -7,4 +7,16 @@
 # Show result from own algorithm.
 # Verify that both results are the same.
 
+print("Welcome to the Factorial Calculator App.")
+print() # blank line
+
+fact_str = input("What number would you like to compute the factorial of? ")
+fact_num = int(fact_str) # convert input string type to integer type
+
+# Create factorial string then print it
+f_string = ""
+for j in range(1, fact_num): 
+    f_string = f_string + str(j) + "*"  # create string of 1*2*3*...* 
+f_string = f_string + str(fact_num)   # add final value of fact_num to string
+print("{}! = {}".format(fact_str, f_string))
 
