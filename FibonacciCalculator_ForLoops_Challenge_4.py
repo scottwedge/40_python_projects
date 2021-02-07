6ebbb67b404c02114995ded6ab14b098c1de5632
Scott Wedge <scott.wedge@gmail.com>
2021-02-06 21:13:44 -0500

    Welcome user and get user input for number of Fibonacci sequence values

--- FibonacciCalculator_ForLoops_Challenge_4.py
+++ FibonacciCalculator_ForLoops_Challenge_4.py
@@ -5,4 +5,11 @@
 # Then calculate the ratio of consecutive Fibonacci numbers.
 # Which should approach 1.618
 
+print("Welcome to the Fibonacci Calculator App.")
+print() # blank line
 
+num = input("How many digits of the Fibonacci Sequence would you likke to compute: ")
+num = int(num) # convert input string to integer
+print() # blank line
+
+print("The first {} numbers of the Fibonacci sequence are: ".format(num))
