f8b2825292dc8ba5b3e42cc9d54201ab6924344e
Scott Wedge <scott.wedge@gmail.com>
2021-02-06 21:04:27 -0500

    Initial commit with comments for each section of code for Fibonacci Sequence calculator app

---
+++ FibonacciCalculator_ForLoops_Challenge_4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Print welcome header.
+# Calculate the first n terms of the Fibonacci sequence.
+# Then calculate the ratio of consecutive Fibonacci numbers.
+# Which should approach 1.618
+
+
