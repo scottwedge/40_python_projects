276da150f510df3d237f695728960ae80d926beb
Scott Wedge <scott.wedge@gmail.com>
2021-02-07 23:27:54 -0500

    Initial commit with description of each step of script

---
+++ GradePointAverage_ForLoops_Challenge_5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Ask for user name
+# Ask for how many grades are to be entered
+# Enter grades
+# Sort grades from highest to lowest
+# Calculate average grade
+# Display grades summary
+# Ask for what average is desired
+# Calculate what grade is needed to obtain that as overall average
+# Change one grade and see how it changes the average
+# Display new summary of grades
+
