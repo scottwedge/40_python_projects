a2171880110b9778162f1cb435435991e2b1d1cb
Scott Wedge <scott.wedge@gmail.com>
2021-02-07 23:33:01 -0500

    Get user name and number of grades to be entered

--- GradePointAverage_ForLoops_Challenge_5.py
+++ GradePointAverage_ForLoops_Challenge_5.py
@@ -11,3 +11,13 @@
 # Change one grade and see how it changes the average
 # Display new summary of grades
 
+
+# Welcome header
+print("Welcome to the Average Calculator App.")
+print() # blank line
+
+name = input("What is your name: ")
+name = name.title() # convert to title format
+num = input("How many grades would you like to enter: ")
+num = int(num) # convert input string type to integer type
+
