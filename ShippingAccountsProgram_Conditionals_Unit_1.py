3393d2dc16fbfd57388b6ee8db758e6626c5a453
Scott Wedge <scott.wedge@gmail.com>
2021-02-09 22:13:44 -0500

    Initial commit with description of each section of code

---
+++ ShippingAccountsProgram_Conditionals_Unit_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Prompt for user name
+# If user in system user list, Welcome user
+# If user not in system, say "sorry you do not have an account and Goodbye"
+# List current shipping prices
+# Prompt for how many items to ship
+# State how much the shipping cost will be
+# Prompt if the order should be places
+# If yes, state shipping the items
