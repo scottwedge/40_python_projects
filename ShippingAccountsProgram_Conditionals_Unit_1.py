3ea1f103b1d3194eccf926aec648b30985242a0d
Scott Wedge <scott.wedge@gmail.com>
2021-02-11 22:21:57 -0500

    Ensure print two decimal places for price '4.80'. It was showing '4.8'"

--- ShippingAccountsProgram_Conditionals_Unit_1.py
+++ ShippingAccountsProgram_Conditionals_Unit_1.py
@@ -76,7 +76,7 @@ def main():
     
         (cost, price) = calc(qty)
     
-        print("To ship {} it will cost you ${:.2f} at ${} per item".format(qty, cost, price))
+        print("To ship {} it will cost you ${:.2f} at ${:.2f} per item".format(qty, cost, price))
     
     print() # blank line
     y_or_n = input("Would you like to place this order (y/n): ")
