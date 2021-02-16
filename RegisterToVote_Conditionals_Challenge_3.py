1d18eeda8abd5abb3da18c99b0a1e1dde0dc6762
Scott Wedge <scott.wedge@gmail.com>
2021-02-16 15:04:31 -0500

    Initial commit with list of steps in the application

---
+++ RegisterToVote_Conditionals_Challenge_3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# A Register To Vote application
+
+# Get user name
+# Get user age
+# Check user age 
+# Give list of parties that one can join
+# Get party (case insensitive) to join from user
+# State user has been registered to that party
+# State whether it is a major party or not
+
+
