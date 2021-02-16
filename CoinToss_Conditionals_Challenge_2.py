9abe5522068f0fb93258a457d2c5ab98311ba1c7
Scott Wedge <scott.wedge@gmail.com>
2021-02-15 22:21:49 -0500

    Initial commit and explanation of each step

---
+++ CoinToss_Conditionals_Challenge_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Welcome user
+# Get number of coin flip attempts to do
+# Ask if want to see results of each flip, or not
+# Display when number of heads = number of tails
+# Display summary of all results for each side: count and percentage
+
