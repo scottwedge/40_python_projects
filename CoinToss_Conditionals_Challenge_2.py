99f3b734df9cc4548f47b469ecde2e476d932d0d
Scott Wedge <scott.wedge@gmail.com>
2021-02-15 22:27:26 -0500

    Welcome user and get number of times to flip coin

--- CoinToss_Conditionals_Challenge_2.py
+++ CoinToss_Conditionals_Challenge_2.py
@@ -6,3 +6,16 @@
 # Display when number of heads = number of tails
 # Display summary of all results for each side: count and percentage
 
+# Functions
+def header():
+    print("Welcome to the Coin Toss App")
+    print() # blank line
+
+def get_number():
+    print("I will flip a coin a set number of times.")
+    num = input("How many times would you like me to flip the coin: ")
+    num = int(num) # Convert input string type to integer type
+    return num
+
+
+header()
