14535a4a0880ebc440a248f0142709210e9929c8
Scott Wedge <scott.wedge@gmail.com>
2021-02-16 19:53:52 -0500

    Initial commit and required steps for Guess My Number app

---
+++ GuessMyNumber_Conditionals_Challenge_4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Welcome user
+# Get user name
+# Generate random number between 1 and 20
+# Start taking number guesses from user
+# Reply if guess is correct, too high or too low.
+# If not correct after five guesses, state value and end game.
+
+
