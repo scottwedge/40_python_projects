5d57af6360d37492894400da298f57d3834fcfb6
Scott Wedge <scott.wedge@gmail.com>
2021-02-17 22:16:15 -0500

    First attempt at deciding who wins the round

--- RockPaperScissors_Conditionals_Challenge_5.py
+++ RockPaperScissors_Conditionals_Challenge_5.py
@@ -34,12 +34,19 @@ def choice(list_of_choices):
 def get_user_choice():
     choice = input("Time to pick...rock, paper, scissors: ")
     choice = choice.lower() # convert to lower case
+    return choice
+
+def computer_wins(computer_score):
+    print("\tComputer wins!")
+    
+def player_winds(player_score):
+    print("\tPlayer wins!")
 
     
 # Setup
 list_of_choices = ["rock", "paper", "scissors"]
-computer_wins = 0
-player_wins = 0
+computer_score = 0
+player_score = 0
 
 welcome()
 num = get_num()
@@ -48,8 +55,25 @@ for j in range(num):
     computer_choice = choice(list_of_choices)
     print() # blank line
     print("Round {}".format(j+1))  # Round number
-    print("Player: {}\tComputer: {}".format(player_wins, computer_wins)) # Current score
+    print("Player: {}\tComputer: {}".format(player_score, computer_score)) # Current score
     player_choice = get_user_choice()
 
     print("\tComputer: {}".format(computer_choice))
     print("\tPlayer: {}".format(player_choice))
+
+    if player_choice not in list_of_choices:
+        print("That is not a valid game option!\nComputer gets the point!")
+        computer_score = computer_score + 1
+    else: # user input is valid
+        if computer_choice == player_choice: # tie scenario
+            print("tIt is a tie, how boring!\n\tThis round was a tie")
+        if computer_choice == "rock":
+            if player_choice == "paper": player_wins(player_score)
+            else: computer_wins(computer_score)
+        if computer_choice == "paper":
+            if player_choice == "scissors": player_wins(player_score)
+            else: computer_wins(computer_score)
+        if computer_choice == "scissors":
+            if player_choice == "rock": player_wins(player_score)
+            else: computer_wins(computer_score)
+        
