01d16808b3fbe4bc3dd52694fa91d4b19c7abce7
Scott Wedge <scott.wedge@gmail.com>
2021-02-18 15:14:19 -0500

    Initial commit with comments for each section of code for Quiz Thesaurus app

---
+++ QuizThesaurus_Dictionaries_Challenge_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Print purpose statement
+# List all words in thesaurus app
+# Have user select which word for a synonym
+# Then list all synonyms for all words in app
+
+
