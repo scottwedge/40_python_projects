d0f588ec648bdaaf2ce4ffbf34f988b2c8128ada
Scott Wedge <scott.wedge@gmail.com>
2021-02-17 20:20:26 -0500

    Create welcome and get number of rounds functions

--- RockPaperScissors_Conditionals_Challenge_5.py
+++ RockPaperScissors_Conditionals_Challenge_5.py
@@ -10,3 +10,20 @@
 # Then select pick and show results
 # If tie then no change in score
 # If invalid choice then computer wins
+
+# Functions
+
+def welcome():
+    print("Welcome to a game of Rock, Paper, Scissors")
+    print() # blank line
+
+def get_num():
+    num = input("How many rounds would you like to play: ")
+    num = int(num)
+    return num
+
+
+
+welcome()
+get_num()
+
