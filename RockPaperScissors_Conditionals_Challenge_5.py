4cc6d33adeeb6f94bf3f0c6e621052913a76e94a
Scott Wedge <scott.wedge@gmail.com>
2021-02-17 19:45:17 -0500

    Initial Rock Paper Scissors app commit with comments for each section of code

---
+++ RockPaperScissors_Conditionals_Challenge_5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Rock Paper Scissors challenge
+
+
+# Welcome user
+# Ask how many rounds are to be played
+# State current 
+# State round number, current score
+# Then select pick and show results
+# If tie then no change in score
+# If invalid choice then computer wins
