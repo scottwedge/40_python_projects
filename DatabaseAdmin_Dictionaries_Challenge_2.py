257dcc6a94ee2ab6768507ff5fcd7de901da9ba7
Scott Wedge <scott.wedge@gmail.com>
2021-02-19 15:09:40 -0500

    Initial commit with list of program requirements as comments

---
+++ DatabaseAdmin_Dictionaries_Challenge_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Database Administrator simulator program
+# Simulate logging into database and prompt user to change password
+# All usernames and passwords will be stored in a dictionary
+# After enter correct credentials, program will prompt user for a new password
+# New password must be at least 8 characters long
+# Accept new password if valid, else reject it
+# If logged in user is an Admin, all the usernames and passwords will be displayed
+
+
