6ea14f1d1a6f896a1830baaf70513bc5f77e7748
Scott Wedge <scott.wedge@gmail.com>
2021-02-20 14:46:12 -0500

    Initial commit with comment for each step of program

---
+++ YesOrNoPolling_Dictionaries_Challenge_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Yes or No Polling Issue
+
+# Conduct a poll on a yes or no issue
+# Prompt user for an issue
+# The number of voters
+# And a password to view the poll results
+# Then conduct the poll
+# Ask for voters name
+# If not yet voted, then display the issue and record the vote
+# Polls close when all have voted
+# Display summary of results
+# If correct password is used, each voter's name and vote are displayed
+
+
