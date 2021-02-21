9bdcef0b0fa836fbe5d5d6f228d48cd46888d913
Scott Wedge <scott.wedge@gmail.com>
2021-02-21 13:41:17 -0500

    Initial commit and comment for each step of program

---
+++ FrequencyAnalysis_Dictinaries_Challenge_4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Analyze the letter distribution of a given text
+# Take any text, 
+# Remove any non-alpha characters,
+# Count the frequency of each letter in the text
+# Calculate the percentage of occurrence of each letter
+# Create a list of letters ordered from highest occurrence to lowest occurrence
+# For two different bodies of text
+
+
