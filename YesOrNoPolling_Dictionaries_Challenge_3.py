2d85b0fc5efee030d39b7f1d9475e7ed903b3163
Scott Wedge <scott.wedge@gmail.com>
2021-02-20 20:05:38 -0500

    Give warning if duplicate name tries to vote

--- YesOrNoPolling_Dictionaries_Challenge_3.py
+++ YesOrNoPolling_Dictionaries_Challenge_3.py
@@ -9,6 +9,7 @@
 # Then conduct the poll
 # Ask for voters name
 # If not yet voted, then display the issue and record the vote
+# If already voted then warn that that name has already voted.
 # Polls close when all have voted
 # Display summary of results
 # If correct password is used, each voter's name and vote are displayed
@@ -56,8 +57,11 @@ password = get_password()
 for j in range(num):
     print() # blank line
     name = get_name()
-    print(name)
-    show_issue(issue)
-    vote = get_vote()
-    voting_db[name] = vote # add vote to db
-    print("Thank you {}! Your vote of {} has been recorded.".format(name, vote))
+    if name in voting_db.keys():
+        print("Sorry, it seems that someone with that name has already voted.")
+    else:
+        show_issue(issue)
+        vote = get_vote()
+        voting_db[name] = vote # add vote to db
+        print("Thank you {}! Your vote of {} has been recorded.".format(name, vote))
+
