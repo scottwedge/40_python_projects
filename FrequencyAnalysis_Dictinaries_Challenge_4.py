53f469563085757089d03d2bc1b75963f4e59f61
Scott Wedge <scott.wedge@gmail.com>
2021-02-21 14:26:52 -0500

    Convert text to lower case; count occurrence of only alphabet

--- FrequencyAnalysis_Dictinaries_Challenge_4.py
+++ FrequencyAnalysis_Dictinaries_Challenge_4.py
@@ -21,3 +21,17 @@ def get_text():
 
 welcome()
 text = get_text()
+
+# Analyze text
+alpha_count_db = {} # initialize blank alphabet dictionary
+text = text.lower() # convert to all lower case
+text_list = list(text) # convert text string to list of characters
+print(text_list)
+
+for j in range(len(text_list)):
+    if text_list[j].isalpha():  # if alphanumeric
+        if text_list[j] in alpha_count_db.keys(): # check if key already exists
+            alpha_count_db[text_list[j]] = alpha_count_db[text_list[j]] + 1 # increment counter
+        else:
+            alpha_count_db[text_list[j]] = 1 # initialize counter
+
