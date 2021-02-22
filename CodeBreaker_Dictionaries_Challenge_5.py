a702afbf2710807ea0466853ec9b48f366331daa
Scott Wedge <scott.wedge@gmail.com>
2021-02-22 16:33:10 -0500

    initial commit with comments for steps of program

---
+++ CodeBreaker_Dictionaries_Challenge_5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Code Breaker app
+
+# Create program that will encode or decode a secret message 
+# based off a letter distribution (see previous Challenge 4 Letter Frequency Distribution program) of a pre-determined key text
+# Determine the frequency analysis of two texts 
+# And use the letter distributions to create a cypher
+# to either encode or decode a text string entered by user
+# 
+# No longer asked to enter key phrase, instead use pre-determined key phrase
+# 
+# Analyze both phrases
+# Decide if encoding or decoding
+# Enter phrase
+# Show encoded message and copy it
+# Then rerun program and select decode and decode that message
+# Verify that it matches original message
