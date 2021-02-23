527eb5b2050f5d8b0c9313b63aca2b50bfb1ad59
Scott Wedge <scott.wedge@gmail.com>
2021-02-22 21:17:29 -0500

    Correct text string name; show missing characters; add missing characters to constant string

--- CodeBreaker_Dictionaries_Challenge_5.py
+++ CodeBreaker_Dictionaries_Challenge_5.py
@@ -23,7 +23,7 @@ import FrequencyAnalysis_Dictinaries_Challenge_4 as fa
 
 
 # Constants
-ISPSUM_TEXT_1 = """
+IPSUM_TEXT_1 = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum orci venenatis est faucibus venenatis. Mauris euismod quam quis neque rutrum cursus. Vivamus at diam massa. Nullam sit amet accumsan purus. Nullam metus turpis, aliquet id pharetra suscipit, fringilla in ex. Aliquam condimentum non lectus non aliquet. Nunc est tellus, vestibulum et ipsum non, mattis varius velit. Integer lobortis turpis in eros ornare lacinia.
 
 Sed mollis, nibh non pellentesque porttitor, dui felis ornare magna, ac elementum erat sapien vitae ante. Ut quis magna mi. Integer quis bibendum sem. Morbi ac purus lorem. Morbi a sem rhoncus, varius metus in, consequat neque. Nulla nisi turpis, bibendum a ante vitae, ornare dictum quam. Integer quis nulla mi. Vivamus varius cursus est, a auctor mi aliquam quis.
@@ -31,11 +31,12 @@ Sed mollis, nibh non pellentesque porttitor, dui felis ornare magna, ac elementu
 Sed augue quam, aliquam at nunc at, sodales sagittis nunc. Sed vel arcu ac mauris mollis blandit non convallis leo. Mauris euismod nulla at eleifend fringilla. Praesent tempus lorem leo, sed consectetur quam varius at. Mauris bibendum ut neque eleifend cursus. Suspendisse pharetra, ante ac tempus consequat, lacus neque hendrerit orci, aliquam pellentesque lectus sapien imperdiet tortor. Cras at urna ac purus rhoncus elementum. Nam condimentum risus ac mattis blandit. In aliquam at lorem vel blandit. Ut maximus orci sapien, consectetur fermentum ex elementum sed. Vestibulum nec tempor dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
 
 Maecenas non ipsum id nunc sollicitudin ullamcorper. Nam vehicula lacinia turpis in fringilla. Nullam vel nisl sed eros pharetra ultrices ut sit amet nunc. Quisque vestibulum elementum nulla nec vulputate. Maecenas quam leo, posuere vel congue sed, dictum et lacus. Donec non aliquam lorem. Nam tempus lacinia nisi nec elementum. Donec sit amet tortor vitae augue pulvinar consectetur vitae non ante. Curabitur condimentum convallis lectus, id pharetra ex ultricies sed. Integer vitae aliquam lacus. In ultrices nisi ac ipsum imperdiet, vel mattis dui hendrerit.
+jkwyz
 """
 
 
 # Functions
-def welcome()
+def welcome():
     return "Welcome to the Code Breakers App"
 
 
@@ -43,9 +44,14 @@ def welcome()
 print(welcome())
 
 # analyze the text
-sorted_db = fa.make_db(ISPUM_TEXT_1)
+sorted_db = fa.make_db(IPSUM_TEXT_1)
 
 # display occurrences of each letter
 print() # blank line
 print("Here is the frequency analysis: ")
 fa.show_occurrence(sorted_db)
+
+# List missing characters missing from analyzed text
+missing = fa.missing_letters(sorted_db)
+print() # blank line
+print("The following letters are missing: {}".format(missing))
