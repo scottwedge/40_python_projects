81120f79024561dd3d328b1adbc036d9d5ad7e15
Scott Wedge <scott.wedge@gmail.com>
2021-02-22 20:27:37 -0500

    Move frequency analysis into function; simplifies module import

--- FrequencyAnalysis_Dictinaries_Challenge_4.py
+++ FrequencyAnalysis_Dictinaries_Challenge_4.py
@@ -23,6 +23,19 @@ def get_text():
     text = input("Enter a word or phrase to count the occurrences of each letter: ")
     return text
 
+def make_db(text):
+    alpha_count_db = {} # initialize blank alphabet dictionary
+    text = text.lower() # convert to all lower case
+    text_list = list(text) # convert text string to list of characters
+        
+    for j in range(len(text_list)):
+        if text_list[j].isalpha():  # if alphanumeric
+            if text_list[j] in alpha_count_db.keys(): # check if key already exists
+                alpha_count_db[text_list[j]] = alpha_count_db[text_list[j]] + 1 # increment counter
+            else:
+                alpha_count_db[text_list[j]] = 1 # initialize counter
+    return alpha_count_db
+
 def show_occurrence(alpha_count_db):
     print() # blank line
     print("\t{:<15} {:<15} {:<15}".format("Letter", "Occurrence", "Percentage"))
@@ -74,16 +87,7 @@ def main():
         text = get_text()
         
         # Analyze text
-        alpha_count_db = {} # initialize blank alphabet dictionary
-        text = text.lower() # convert to all lower case
-        text_list = list(text) # convert text string to list of characters
-        
-        for j in range(len(text_list)):
-            if text_list[j].isalpha():  # if alphanumeric
-                if text_list[j] in alpha_count_db.keys(): # check if key already exists
-                    alpha_count_db[text_list[j]] = alpha_count_db[text_list[j]] + 1 # increment counter
-                else:
-                    alpha_count_db[text_list[j]] = 1 # initialize counter
+        alpha_count_db = make_db(text)
         
         # print results
         print() # blank line
