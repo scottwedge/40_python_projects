02b380b0611e7d8602d6a6939a90f26181fe4ac7
Scott Wedge <scott.wedge@gmail.com>
2021-02-22 21:43:33 -0500

    Add second constant text phrase; Duplicate steps to analyze second text phrase; prompt for encode or decode; prompt for secret phrase

--- CodeBreaker_Dictionaries_Challenge_5.py
+++ CodeBreaker_Dictionaries_Challenge_5.py
@@ -34,24 +34,97 @@ Maecenas non ipsum id nunc sollicitudin ullamcorper. Nam vehicula lacinia turpis
 jkwyz
 """
 
+SHERLOCK_HOLMES_2 = """
+1. “You have a grand gift for silence, Watson. It makes you quite invaluable as a companion.” — Arthur Conan Doyle, The Man with the Twisted Lip
+
+2. “Oh, hell! What does that matter?! So we go around the sun! If we went around the moon or round and round the garden like a teddy bear, it wouldn’t make any difference! All that matters to me is the work! Without that, my brain rots. Put that in your blog — or better still, stop inflicting your opinions on the world!” — Sherlock BBC series (spoken by Benedict Cumberbatch)
+
+3. “You see, but you do not observe.” — Arthur Conan Doyle, A Scandal in Bohemia 
+
+4.“It is a capital mistake to theorize before one has data. Insensibly one begins to twist facts to suit theories, instead of theories to suit facts.” — Arthur Conan Doyle, A Scandal in Bohemia
+
+5.  “When you have eliminated the impossible, whatever remains, however improbable, must be the truth?” — Arthur Conan Doyle, The Sign of Four
+
+6. “Watson. Come at once if convenient. If inconvenient, come all the same.” — Arthur Conan Doyle, The Adventure of the Creeping Man
+
+7. “Dear God. What is it like in your funny little brains? It must be so boring.” —Sherlock BBC Series (spoken by Benedict Cumberbatch)
+
+8. “To a great mind, nothing is little.” — Arthur Conan Doyle, A Study in Scarlet
+
+9. “Crime is common. Logic is rare.” — Arthur Conan Doyle, The Adventure of the Copper Beeches
+
+10. “The devil’s due a soul, I’d say.” — Sherlock Holmes 2009 Film Adaptation (spoken by Robert Downey Jr.)
+
+11. “My name is Sherlock Holmes. It is my business to know what other people do not know.” — Arthur Conan Doyle, The Adventure of the Blue Carbuncle
+
+12. “Ive always assumed that love is a dangerous disadvantage. Thank you for the final proof.” — Sherlock BBC Series (spoken by Benedict Cumberbatch)
+
+13. “I abhor the dull routine of existence. I crave for mental exaltation. That is why I have chosen my own particular profession, or rather created it, for I am the only one in the world.” — Arthur Conan Doyle, The Sign of the Four
+
+14. “A good detective knows that every task, every interaction, no matter how seemingly banal, has the potential to contain multitudes.” — Elementary CBS Series (spoken by Jonny Lee Miller)
+
+15. “The game is afoot.” — Arthur Conan Doyle, The Adventure of the Abbey Grange
+"""
+
 
 # Functions
 def welcome():
     return "Welcome to the Code Breakers App"
 
+def encode_or_decode():
+    print() # blank line
+    code = input("Would you like to encode or decode a message: ")
+    code = code.lower()
+    code = code.rstrip()
+    return code
+
+def get_phrase():
+    print() # blank line
+    phrase = input("What is the phrase: ")
+    return phrase
 
 # Main program
 print(welcome())
 
-# analyze the text
-sorted_db = fa.make_db(IPSUM_TEXT_1)
+# analyze the text of phrase #1
+db_1 = fa.make_db(IPSUM_TEXT_1)
 
 # display occurrences of each letter
 print() # blank line
-print("Here is the frequency analysis: ")
-fa.show_occurrence(sorted_db)
+print("Here is the frequency analysis of string #1: ")
+fa.show_occurrence(db_1)
 
 # List missing characters missing from analyzed text
-missing = fa.missing_letters(sorted_db)
+missing = fa.missing_letters(db_1)
 print() # blank line
 print("The following letters are missing: {}".format(missing))
+
+# List characters in descending order of occurrence
+descending_string = fa.descending_order(db_1)
+print() # blank line
+print("Characters in descending order are: {}".format(descending_string))
+
+# analyze the text of phrase #2
+db_2 = fa.make_db(SHERLOCK_HOLMES_2)
+
+# display occurrences of each letter
+print() # blank line
+print("Here is the frequency analysis of string #2: ")
+fa.show_occurrence(db_2)
+
+# List missing characters missing from analyzed text
+missing = fa.missing_letters(db_2)
+print() # blank line
+print("The following letters are missing: {}".format(missing))
+
+# List characters in descending order of occurrence
+descending_string = fa.descending_order(db_2)
+print() # blank line
+print("Characters in descending order are: {}".format(descending_string))
+
+# Determine if user wants to code or decode
+code = encode_or_decode()
+
+# Get phrase to encode or decode
+phrase = get_phrase()
+ 
