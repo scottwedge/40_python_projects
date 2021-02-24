9486841b49d47ff4acd1a03263ef9478115046b1
Scott Wedge <scott.wedge@gmail.com>
2021-02-24 16:29:37 -0500

    Welcome user, get number to factor, list factors of that number

--- FactorGenerator_While_Challenge_1.py
+++ FactorGenerator_While_Challenge_1.py
@@ -7,4 +7,36 @@
 # Show all factors
 # Ask if user wants to factor another number
 
+# Constants
 
+# Variables
+main_loop = True # main while loop
+factor_loop = True # loop that lists factors
+factor_list = []
+
+
+# Imports
+
+# Functions
+def welcome():
+    return ("Welcome to the Factor Generator App")
+
+def get_number():
+    print() # blank line
+    num = input("Enter a number to determine all factors of that number: ")
+    num = int(num) # convert from string type to integer type
+    return num
+
+def list_factors(num):
+    print() # blank line
+    print("Factors of {} are:".format(num))
+    for j in range(1, num+1):
+        if num % j == 0:
+            print(j)
+    
+# Main code
+print(welcome())
+
+while main_loop:
+    num = get_number()
+    list_factors(num)
