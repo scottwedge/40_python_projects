8cea19ab8a84f4dd6125e51d294aa35f2b3eccc5
Scott Wedge <scott.wedge@gmail.com>
2021-02-25 15:44:20 -0500

    Initial commit for even or odd number sorting app with comment for each section of code

---
+++ EvenOddNumberSorter_While_Challenge_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Welcome user
+# get list of numbers separated by commas
+# List each number and state if odd or even
+# Then list all even numbers
+# Then list all odd numbers
+# Then ask if user wants to do this again
+# If no, thank user
