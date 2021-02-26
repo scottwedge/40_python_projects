8f45ffd42bfafd7926df0cb26b6cb9a513e89b04
Scott Wedge <scott.wedge@gmail.com>
2021-02-26 14:30:44 -0500

    Check if number is prime or not

--- PrimeNumber_While_Challenge_3.py
+++ PrimeNumber_While_Challenge_3.py
@@ -8,6 +8,7 @@
 # Prompt user to enter choice
 # Check validity of choice; warn user if invalid and prompt again for input
 # Enter input
+# Record how long it took to determine results
 # Display results
 # Ask user if want to repeat program
 
@@ -27,8 +28,26 @@ def get_option():
     option = input("Enter your choice 1 or 2: ")
     return option
 
+def check_if_prime(user_num):
+    is_prime = True # initialize result
+    for j in range(2, user_num):
+        if user_num % j == 0: # check if number is divisible
+            is_prime = False
+    return is_prime
+
 # Main code
 welcome()
 give_options()
 option = get_option()
+
+if option == "1":
+    print() # blank line
+    user_num = input("Enter a number to determine if it is prime or not: ")
+    user_num = int(user_num)
+    is_prime = check_if_prime(user_num)
+
+if is_prime:
+    print("{} is prime!".format(user_num))
+else:
+    print("{} is not prime!".format(user_num))
 
