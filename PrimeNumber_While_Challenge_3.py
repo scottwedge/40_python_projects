99bb3b768fa21ad469d0cf8831ccd1381d5588bf
Scott Wedge <scott.wedge@gmail.com>
2021-02-26 13:49:51 -0500

    Initial commit and comments for each stage of Prime Number app

---
+++ PrimeNumber_While_Challenge_3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Prime Number App
+
+# Welcome user
+# Print Option #1 to determine if a number is prime
+# Print option #2 to determine all prime numbers within a set range
+# Prompt user to enter choice
+# Check validity of choice; warn user if invalid and prompt again for input
+# Enter input
+# Display results
+# Ask user if want to repeat program
+
