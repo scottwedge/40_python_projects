dd92d90bec68e97338b34e19fa5a6c271fd4244e
Scott Wedge <scott.wedge@gmail.com>
2021-02-28 20:09:59 -0500

    Move some main code into functions to simplify future testing

--- FibonacciCalculator_ForLoops_Challenge_4.py
+++ FibonacciCalculator_ForLoops_Challenge_4.py
@@ -13,16 +13,13 @@ def header():
 def fib(last, second_last):
     return last + second_last
 
-def main():
-    header()    
- 
+def get_num():
     num = input("How many digits of the Fibonacci Sequence would you like to compute: ")
     num = int(num) # convert input string to integer
     print() # blank line
-    
-    print("The first {} numbers of the Fibonacci sequence are: ".format(num))
-    
-    # Create list of first num Fibonacci sequence, then display it.
+    return num
+
+def create_fib_list(num):
     fib_list = [1, 1] # most common case
     if num == 1:
         fib_list = [1]
@@ -30,10 +27,23 @@ def main():
         for j in range(3, num+1):
             fib_list.append(fib(fib_list[-1], fib_list[-2]))
     else:
-        print("number must be integer greater than zero")
+        fib_list=[]
+    return fib_list
+
+def main():
+    header()    
+ 
+    num = get_num()
+ 
     
-    for j in range(num): 
-        print(fib_list[j])  # print Fibonacci number from list
+    # Create list of first num Fibonacci sequence, then display it.
+    fib_list = create_fib_list(num)
+    if fib_list == []:
+        print("number must be integer greater than zero")
+    else:
+        print("The first {} numbers of the Fibonacci sequence are: ".format(num))
+        for j in range(num): 
+            print(fib_list[j])  # print Fibonacci number from list
     
     print() # blank line
     print("The corresponding Golden Ratios are: ")
