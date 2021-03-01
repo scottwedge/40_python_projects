23bd36aaf7a3fcb163c7d445ccd19d2939e564a0
Scott Wedge <scott.wedge@gmail.com>
2021-02-28 21:02:18 -0500

    Move more code into functions; Add more tests

--- FibonacciCalculator_ForLoops_Challenge_4.py
+++ FibonacciCalculator_ForLoops_Challenge_4.py
@@ -13,6 +13,9 @@ def header():
 def fib(last, second_last):
     return last + second_last
 
+def test_fib():
+    assert fib(21, 34) == 55
+
 def get_num():
     num = input("How many digits of the Fibonacci Sequence would you like to compute: ")
     num = int(num) # convert input string to integer
@@ -30,12 +33,24 @@ def create_fib_list(num):
         fib_list=[]
     return fib_list
 
+def test_create_fib_list():
+    assert create_fib_list(10) == [1,1,2,3,5,8,13,21,34,55]
+
+def calculate_golden_ratio(num, fib_list):
+    gr = 1 # initialize return value in case num <= 2
+    for j in range(2, num+1):
+        gr = fib_list[j-1]/fib_list[j-2]
+    return gr
+
+def test_calculate_golden_ratio():
+    assert calculate_golden_ratio(6, [1,1,2,3,5,8]) == 1.6
+
+
 def main():
     header()    
  
     num = get_num()
  
-    
     # Create list of first num Fibonacci sequence, then display it.
     fib_list = create_fib_list(num)
     if fib_list == []:
@@ -48,9 +63,8 @@ def main():
     print() # blank line
     print("The corresponding Golden Ratios are: ")
     # Calculate Golden Ratio (last number divided by second last number) and print
-    for j in range(2, num+1):
-        gr = fib_list[j-1]/fib_list[j-2]
-        print(gr)
+    gr = calculate_golden_ratio(num, fib_list)
+    print(gr)
     
     print() # blank line
     print("The ratio of consecutive Fibonacci terms approaches Phi: 1.618...")
