2d9ae89d4ad8725df7406d2b67bd48ab0fa266a2
Scott Wedge <scott.wedge@gmail.com>
2021-03-01 22:53:39 -0500

    Generate list of available white balls; Select white balls; Select red balls; Combine balls and display them

--- PowerBallSimulator_While_Challenge_4.py
+++ PowerBallSimulator_While_Challenge_4.py
@@ -15,6 +15,7 @@
 
 # Imports
 from math import factorial as fact
+import random
 
 # Functions
 def intro():
@@ -38,6 +39,37 @@ def get_ticket_purchase_interval():
     interval = int(input("Purchase tickets in what interval: "))
     return interval
 
+def generate_available_wb_list(count):
+    available_wb = []
+    for j in range(1, count+1):
+        available_wb.append(j)
+    return available_wb
+    
+def pick_winning_white(white_bc):
+    winner_white = []
+    available_wb = generate_available_wb_list(white_bc)
+    for j in range(5):
+        random.seed()
+        l = len(available_wb)
+        list_index = random.randint(0,l-1)  
+        chosen_ball = available_wb[list_index]
+        available_wb.remove(chosen_ball)   # remove selected ball from available list
+        winner_white.append(chosen_ball) # add selected ball to winner list
+    winner_white.sort()  # sort white balls into numerical order
+    return winner_white
+
+def pick_winning_red(red_bc):
+    random.seed()
+    winner_red = random.randint(1,red_bc)
+    return winner_red
+
+def pick_winning_white_and_red_combo(white_bc, red_bc):
+    winner = pick_winning_white(white_bc)
+    winner_red = pick_winning_red(red_bc)
+    winner.append(winner_red)  # combine white and red winners
+    return winner
+
+
 def main():
     print(intro())
     white_bc = get_white_ball_count()
@@ -45,6 +77,14 @@ def main():
     odds = calculate_odds(white_bc, red_bc)
     print("You have a one in {} chance of winning this lottery.".format(odds))
     interval = get_ticket_purchase_interval()
+
+    # Generate winning combination (and sort numerically)
+    winner = pick_winning_white_and_red_combo(white_bc, red_bc)
+    print() # blank line
+    print("Tonight's winning numbers are:",winner)
+
+    # Start buying unique tickets
+
 
 
 
