511426636e3f63af09b1be68c0bd14467a565307
Scott Wedge <scott.wedge@gmail.com>
2021-03-01 20:30:07 -0500

    Initial commit for PowerBall simulator; Comment for each step of code

---
+++ PowerBallSimulator_While_Challenge_4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# PowerBall simulator
+
+# Print title
+# Prompt user how many white balls to choose (normally 69)
+# Prompt user how many red balls to choose (normally 26)
+# Calculate odds
+# Ask user how many tickets to purchase each interval
+# Start game and welcome user
+# Print winning numbers
+# Press enter to start purchasing tickets
+# Print tickets
+# Must ensure tickets are unique
