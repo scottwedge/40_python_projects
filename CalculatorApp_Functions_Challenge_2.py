be6533f222583ee62e5e279f73307539e9fd7a1e
Scott Wedge <scott.wedge@gmail.com>
2021-03-03 18:08:10 -0500

    Initial commit and explanation of each step of program

---
+++ CalculatorApp_Functions_Challenge_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Calculator App
+
+# Welcome user
+# Ask for two numbers
+# Ask for operation - accept either first letter or full word
+# Print lexical (?) of operation
+# Ask if want to repeat
+
+
