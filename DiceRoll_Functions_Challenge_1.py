9a390da1633ce6bf94457041a724ee2d9f27deab
Scott Wedge <scott.wedge@gmail.com>
2021-03-02 20:37:50 -0500

    Initial commit and comment for each stage of program

---
+++ DiceRoll_Functions_Challenge_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Dice Roll App
+
+# Get user input for number of sides of each dice
+# Get user input for number of dice
+# State number of dice and number of sides of dice
+# List results for each dice
+# Sum the results of all dice
+# Ask if want to roll again (and start at beginning again)
+
+
