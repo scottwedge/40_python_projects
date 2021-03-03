2f10e707cac056ec56a7a8a9ff376ae11097eef9
Scott Wedge <scott.wedge@gmail.com>
2021-03-02 21:16:47 -0500

    Show roll results; Show sum of rolls; Decide if roll again or not; Thank you if not roll again

--- DiceRoll_Functions_Challenge_1.py
+++ DiceRoll_Functions_Challenge_1.py
@@ -15,10 +15,10 @@ import random
 
 # Functions
 def welcome():
-    return "Welcome to the Python Dice App"
+    print("Welcome to the Python Dice App")
+    print() # blank line
 
 def get_dice_sides():
-    print() # blank line
     print() # blank line
     num_sides = int(input("How many sides would you like on your dice: "))
     return num_sides
@@ -36,10 +36,40 @@ def roll_dice(num_sides, num_dice):
     print("You rolled {} {} sided dice.".format(num_dice, num_sides))
     return results
 
+def display_results(results):
+    print() # blank line
+    print("----- Results are as follows -----")
+    for j in results:
+        print("\t{}".format(j))
+
+def sum_results(results):
+    sum = 0
+    for j in results:
+        sum = sum + j
+    print("The total value of your roll is {}.".format(sum))
+
+def roll_again():
+    print() # blank line
+    answer = input("Would you like to roll again (y/n): ")
+    answer = answer.lower()
+    answer = answer.rstrip()
+    if answer == "y" or answer == "yes":
+        return True
+    else:
+        return False
+
+def thankyou():
+    print("Thank you for using the Python Dice App.")
 
 # Main code
-print(welcome())
-num_sides = get_dice_sides()
-num_dice = get_number_of_dice()
-results = roll_dice(num_sides, num_dice)
+welcome()
+loop_forever = True
 
+while loop_forever:
+    num_sides = get_dice_sides()
+    num_dice = get_number_of_dice()
+    results = roll_dice(num_sides, num_dice)
+    display_results(results)
+    sum_results(results)
+    loop_forever = roll_again()
+thankyou()
