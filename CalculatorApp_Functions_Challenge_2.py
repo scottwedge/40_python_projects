ee34c9b8e341c6b05b3f6482d0a5a212efeef520
Scott Wedge <scott.wedge@gmail.com>
2021-03-03 19:41:19 -0500

    Get operation; Print operation and result

--- CalculatorApp_Functions_Challenge_2.py
+++ CalculatorApp_Functions_Challenge_2.py
@@ -10,17 +10,36 @@
 
 # Functions
 def welcome():
-    print("Welcome to the Python Applicator App"
+    print("Welcome to the Python Applicator App")
     print("Enter two numbers and the desired operation")
 
 def get_number():
     num = input("Enter a number: ")
+    num = float(num)  # convert from string type to float type
     return num
 
+def get_operation():
+    op = input("Enter an operation (addition, subtraction, multiplication, division or exponentiation): ")
+    op = op.lower()
+    op = op.rstrip()
+    return op
 
 
 # Main code
 welcome()
-first = get_num()
-second = get_num()
+first = get_number()
+second = get_number()
+op = get_operation()
 
+if op == "addition" or op == "a":
+    print("The sum of {} and {} is {}.".format(first, second, first + second))
+elif op == "subtraction" or op == "s":
+    print("The result of {} minus {} is {}.".format(first, second, first - second))
+elif op == "multiplication" or op == "m":
+    print("The product of {} and {} is {}.".format(first, second, first * second))
+elif op == "division" or op == "d":
+    print("The result of {} divied by {} is {}.".format(first, second, first / second))
+elif op == "exponentiation" or op == "e":
+    print("The result of {} to power of {} is {}.".format(first, second, first ** second))
+else:
+    print("ERROR")
