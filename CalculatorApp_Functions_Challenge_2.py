88854eec4abf6cd606925948e44d5594a8c35f8a
Scott Wedge <scott.wedge@gmail.com>
2021-03-03 19:22:29 -0500

    Welcome user; Get two numbers

--- CalculatorApp_Functions_Challenge_2.py
+++ CalculatorApp_Functions_Challenge_2.py
@@ -8,4 +8,19 @@
 # Print lexical (?) of operation
 # Ask if want to repeat
 
+# Functions
+def welcome():
+    print("Welcome to the Python Applicator App"
+    print("Enter two numbers and the desired operation")
+
+def get_number():
+    num = input("Enter a number: ")
+    return num
+
+
+
+# Main code
+welcome()
+first = get_num()
+second = get_num()
 
