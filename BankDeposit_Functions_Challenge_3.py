b61cf3d4182b155024ed7947b05572320a80e9db
Scott Wedge <scott.wedge@gmail.com>
2021-03-04 14:11:11 -0500

    Initial commit of Bank Deposit app; Comment for each step of program

---
+++ BankDeposit_Functions_Challenge_3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Bank Deposit App
+
+# Welcome user
+# set name
+# Setup balance in Savings Account
+# Setup balance in Chequing Account
+# Allowed to deposit
+# Allowed to withdraw if not get negative balance
+# Display current account info after every event
+# Offer to perform another transaction
+
+
