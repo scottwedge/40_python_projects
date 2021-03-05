9d10a67dbe683e4ccf646ea82132db3d98cedbf2
Scott Wedge <scott.wedge@gmail.com>
2021-03-05 14:36:51 -0500

    Initial commit with steps of program

---
+++ TicTacToe_Functions_Challenge_4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 
+
+# Tic Tac Toe game
+
+# Display board with positions numbered
+# above board with taken (X or O) and free ('_') positions
+# Alternate between users
+# Three in a row wins
+
