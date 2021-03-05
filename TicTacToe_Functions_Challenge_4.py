a287e977213b2a424192a73a528b42ef39e9f7d6
Scott Wedge <scott.wedge@gmail.com>
2021-03-05 15:05:24 -0500

    Create board template then create two boards based on it

--- TicTacToe_Functions_Challenge_4.py
+++ TicTacToe_Functions_Challenge_4.py
@@ -7,3 +7,34 @@
 # Alternate between users
 # Three in a row wins
 
+
+# Functions
+def draw_board(g):
+    print() # blank line
+    print("\t   Tic-Tac-Toe")
+    print("\t-----------------")
+    print("\t|| {} || {} || {} ||".format(g[0], g[1], g[2]))
+    print("\t-----------------")
+    print("\t|| {} || {} || {} ||".format(g[3], g[4], g[5]))
+    print("\t-----------------")
+    print("\t|| {} || {} || {} ||".format(g[6], g[7], g[8]))
+    print("\t-----------------")
+
+def position_board():
+    draw_board("123456789")
+
+def game_board(guesses):
+    draw_board(guesses)
+
+def get_side(previous_side):
+    return not previous_side
+
+# Main code
+# Init variables
+guesses = "_________"
+side_X = True
+run_forever = True  # while loop boolean
+
+while run_forever:
+    position_board()
+    game_board(guesses)
