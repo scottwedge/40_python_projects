7c1e49026d3425b492b0505e947710cbdd959ceb
Scott Wedge <scott.wedge@gmail.com>
2021-03-05 23:23:08 -0500

    Check all combinations for a winner

--- TicTacToe_Functions_Challenge_4.py
+++ TicTacToe_Functions_Challenge_4.py
@@ -65,6 +65,27 @@ def place_piece(side_X, guesses):
             loop_again = True
     return guesses
 
+def three_in_a_row(g):  # determine if anyone has three in a row (and wins)
+    if g[0] == g[1] == g[2] == "O" or \
+    g[3] == g[4] == g[5] == "O" or \
+    g[6] == g[7] == g[8] == "O" or \
+    g[0] == g[3] == g[6] == "O" or \
+    g[1] == g[4] == g[7] == "O" or \
+    g[2] == g[5] == g[8] == "O" or \
+    g[0] == g[4] == g[8] == "O" or \
+    g[6] == g[4] == g[2] == "O":
+       return True
+    if g[0] == g[1] == g[2] == "X" or \
+    g[3] == g[4] == g[5] == "X" or \
+    g[6] == g[7] == g[8] == "X" or \
+    g[0] == g[3] == g[6] == "X" or \
+    g[1] == g[4] == g[7] == "X" or \
+    g[2] == g[5] == g[8] == "X" or \
+    g[0] == g[4] == g[8] == "X" or \
+    g[6] == g[4] == g[2] == "X":
+       return True
+        
+
 # Main code
 # Init variables
 guesses = init_guesses()
@@ -78,5 +99,9 @@ while run_forever:
     game_board(guesses)
     if all_spots_taken(guesses):
         break
+    winner = three_in_a_row(guesses)
+    if winner:
+        print(" someone WINS!!")
+        break    
     side_X = switch_sides(side_X)
     guesses = place_piece(side_X, guesses)
