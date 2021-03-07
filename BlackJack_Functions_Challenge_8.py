9452fcf6968ee50e37f79a90a435fd606ef0698a
Scott Wedge <scott.wedge@gmail.com>
2021-03-06 19:46:28 -0500

    Initial commit for Black Jack app; Comment for each step of game

---
+++ BlackJack_Functions_Challenge_8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Black Jack application
+
+# Welcome user
+# State miniumum bet
+# Ask how much money to play with
+# State current balance
+# Ask what the bet is (minimum of $20)
+# State current money and current bet
+# Show dealer card
+# Show player's two card hand and total
+# Ask if want to hit
+# If no, state how many cards dealer has
+# Press enter to show dealer cards
+# List dealers and total and who won
