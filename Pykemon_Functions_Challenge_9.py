ed0560253910baf6a7cf5a8fc816c8d46db1b0fd
Scott Wedge <scott.wedge@gmail.com>
2021-03-10 22:30:52 -0500

    Initial commit for Pykemon game with very basic comments for initial steps

---
+++ Pykemon_Functions_Challenge_9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Pykemon game
+
+# Lots of Pokemon terms and assumed knowledge
+
+# Steps:
+# Welcome user
+# Display settings for three potential choices
+# Choose one pykemon
+
+
