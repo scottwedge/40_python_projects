75d6b0b156512190350068f68b308509a5d466bd
Scott Wedge <scott.wedge@gmail.com>
2021-03-10 22:33:49 -0500

    Initial welcome statement

--- Pykemon_Functions_Challenge_9.py
+++ Pykemon_Functions_Challenge_9.py
@@ -10,3 +10,13 @@
 # Choose one pykemon
 
 
+# Functions
+def welcome():
+print("Welcome to Pykemon!"
+print("Can you become the world's greatest Pykemon Trainer???"
+print()
+print("Don't worry! Prof Eramo is here to help you on your quest.")
+print("He would like to gift you your first Pykemon!")
+print("Here are three potential Pykemon partners")
+print("Press Enter to choose your Pykemon!")
+
