3f5eca857988d3cfce50b72fde04907c8d949460
Scott Wedge <scott.wedge@gmail.com>
2021-03-11 20:29:02 -0500

    Initial commit of Pandemic Outbreak terminal application; Comments for each section of code

---
+++ PandemicOutbreakTerminal_Functions_Challenge_20.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Pandemic Outbreak Terminal Application
+
+# Get population size
+# Get percentage of population to initially infect
+# Get probability that a person gets infected when exposed
+# Get how long the infection lasts when exposed
+# Get mortality rate of the infection
+# Get how long to run the simulation
+
+
