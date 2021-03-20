bffb8f41fc88cdf5fbd5101944fd98036c4f434d
Scott Wedge <scott.wedge@gmail.com>
2021-03-19 23:28:32 -0400

    Move main code into 'main'; Now must add more parameters to functions

--- PandemicOutbreakTerminal_Functions_Challenge_20.py
+++ PandemicOutbreakTerminal_Functions_Challenge_20.py
@@ -85,7 +85,7 @@ def format_daily_status(pop):
         status = status + SPACER + str(pop[j])
     return status      
 
-def update_user(pop, days_sick):
+def update_user(pop, new_pop, days_sick, new_days_sick, mort_pc, duration):
     for j in range(len(pop)):
         # check status of user themself
         if pop[j] == DEAD:    # if user was dead then remains dead
@@ -105,7 +105,7 @@ def update_user(pop, days_sick):
             new_pop[j] = pop[j]    # still healthy
     return (new_pop, new_days_sick)
 
-def check_neighbour(pop, new_pop, days_sick, new_days_sick, offset):
+def check_neighbour(pop, new_pop, days_sick, new_days_sick, offset, expose_pc):
 # if offset = -1 then check if lower neighbour infected
 # if offset = 1 then check if upper neighbour infected
     for j in range(len(pop)):
@@ -124,63 +124,67 @@ def check_neighbour(pop, new_pop, days_sick, new_days_sick, offset):
     return (new_pop, new_days_sick)
 
 # Main code
-# Get all inputs from user
-pop_count = get_pop_count()
-pop_pc = get_pop_pc()
-expose_pc = get_exposure_pc()
-duration = get_duration()
-mort_pc = get_mort_pc()
-num_days = get_num_days()
-
-# Initialize all users to be healthy with zero days sick
-pop = init_pop(pop_count, HEALTHY)
-days_sick = init_days_sick(pop_count)
-
-day = 1
-# Calculate number of infections on day 1
-infected_count = pop_count * pop_pc / 100
-infected_count = int(infected_count)   # convert to integer (by rounding down)
-
-# choose randomly who gets infected
-while infected_count > 0:
-    random.seed()
-    infected_index = random.randint(0, pop_count - 1)  # select random person
-    if pop[infected_index]  == HEALTHY:
-        pop[infected_index] = INFECTED  # mark person as infected
-        days_sick[infected_index] += 1
-        infected_count = infected_count - 1
-    else:
-#        print("Ignoring index {} since they are already {}".format(infected_index, pop[infected_index]))
-        pass
-
-status = format_daily_status(pop)
-print("INITIAL STATUS #{:2d}: {}".format(day, status))   # print day #1 health status
-
-
-while day <= num_days: 
-    day = day + 1
+def main():
+    # Get all inputs from user
+    pop_count = get_pop_count()
+    pop_pc = get_pop_pc()
+    expose_pc = get_exposure_pc()
+    duration = get_duration()
+    mort_pc = get_mort_pc()
+    num_days = get_num_days()
+    
+    # Initialize all users to be healthy with zero days sick
+    pop = init_pop(pop_count, HEALTHY)
+    days_sick = init_days_sick(pop_count)
+    
+    day = 1
+    # Calculate number of infections on day 1
+    infected_count = pop_count * pop_pc / 100
+    infected_count = int(infected_count)   # convert to integer (by rounding down)
+    
+    # choose randomly who gets infected
+    while infected_count > 0:
+        random.seed()
+        infected_index = random.randint(0, pop_count - 1)  # select random person
+        if pop[infected_index]  == HEALTHY:
+            pop[infected_index] = INFECTED  # mark person as infected
+            days_sick[infected_index] += 1
+            infected_count = infected_count - 1
+        else:
+    #        print("Ignoring index {} since they are already {}".format(infected_index, pop[infected_index]))
+            pass
+    
     status = format_daily_status(pop)
-#    print("                DAILY STATUS #{:2d}: {}".format(day, status))   # print current day's health status
+    print("INITIAL STATUS #{:2d}: {}".format(day, status))   # print day #1 health status
+    
+    
+    while day <= num_days: 
+        day = day + 1
+        status = format_daily_status(pop)
+    #    print("                DAILY STATUS #{:2d}: {}".format(day, status))   # print current day's health status
+    
+        new_pop = init_pop(pop_count, HEALTHY)     # set all of next day's status to HEALTHY by default
+        new_days_sick = init_days_sick(pop_count)   # set all of next day's sick day count to zero
+    
+        (new_pop, new_days_sick) = update_user(pop, new_pop, days_sick, new_days_sick, mort_pc, duration)   # update user based on previous health setting
+    #    status = format_daily_status(new_pop)
+    #    print("   UPDATED USER DAILY STATUS #{:2d}: {}".format(day, status))
+    
+        # if user healthy, determine if lower neighbour is infected so might infect this user
+        offset = -1    # check lower neighbour
+        (new_pop, new_days_sick) = check_neighbour(pop, new_pop, days_sick, new_days_sick, offset, expose_pc)
+    #    status = format_daily_status(new_pop)
+    #    print("LOWER NEIGHBOUR DAILY STATUS #{:2d}: {}".format(day, status))
+    
+        # if user healthy, determine if upper neighbour is infected so might infect this user
+        offset = 1    # check upper neighbour
+        (new_pop, new_days_sick) = check_neighbour(pop, new_pop, days_sick, new_days_sick, offset, expose_pc)
+        status = format_daily_status(new_pop)
+        print("  DAILY STATUS #{:2d}: {}".format(day, status))
+    
+    
+        pop = new_pop.copy()  # overwrite old health status results with latest results
+        days_sick = new_days_sick.copy()  # overwrite old sick days count with latest values
 
-    new_pop = init_pop(pop_count, HEALTHY)     # set all of next day's status to HEALTHY by default
-    new_days_sick = init_days_sick(pop_count)   # set all of next day's sick day count to zero
-
-    (new_pop, new_days_sick) = update_user(pop, days_sick)   # update user based on previous health setting
-#    status = format_daily_status(new_pop)
-#    print("   UPDATED USER DAILY STATUS #{:2d}: {}".format(day, status))
-
-    # if user healthy, determine if lower neighbour is infected so might infect this user
-    offset = -1    # check lower neighbour
-    (new_pop, new_days_sick) = check_neighbour(pop, new_pop, days_sick, new_days_sick, offset)
-#    status = format_daily_status(new_pop)
-#    print("LOWER NEIGHBOUR DAILY STATUS #{:2d}: {}".format(day, status))
-
-    # if user healthy, determine if upper neighbour is infected so might infect this user
-    offset = 1    # check upper neighbour
-    (new_pop, new_days_sick) = check_neighbour(pop, new_pop, days_sick, new_days_sick, offset)
-    status = format_daily_status(new_pop)
-    print("  DAILY STATUS #{:2d}: {}".format(day, status))
-
-
-    pop = new_pop.copy()  # overwrite old health status results with latest results
-    days_sick = new_days_sick.copy()  # overwrite old sick days count with latest values
+if __name__ == "__main__":
+    main()
